93569d6246be346d6f12c99ddda8e554628e6b9f
Sergey Mysov <s.mysov@list.ru>
2021-03-21 19:00:47 +0400

    refactor: moved products data to vuex store

--- src/store/shop.js
+++ src/store/shop.js
@@ -1,6 +1,9 @@
+import axios from '@/plugins/axios';
+
 export default {
   state: {
     products: [],
+    loading: true,
   },
   mutations: {
     GET_PRODUCTS(state, payload) {
@@ -8,13 +11,29 @@ export default {
     },
   },
   actions: {
-    getProducts({ commit }, payload) {
-      commit('GET_PRODUCTS', payload);
+    async getProducts({ commit, state }) {
+      state.loading = true;
+      try {
+        const response = await axios.get('products/products.json');
+        commit('GET_PRODUCTS', response.data);
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, 2000);
+        });
+      } catch (e) {
+        console.log(e.response.data);
+      } finally {
+        state.loading = false;
+      }
     },
   },
   getters: {
     getProducts(state) {
       return state.products;
+    },
+    getLoading(state) {
+      return state.loading;
     },
   },
 };
